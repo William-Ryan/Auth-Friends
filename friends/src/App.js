0d0626382f9f8185739d1abfa3480129f226f655
William Ryan <billy.ryan.rom@gmail.com>
2020-02-26 14:44:59 -0500

    Add conditonal rendering for links/Add Logout function to Application

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Link, Route } from "react-router-dom"
+import { getToken } from "./utils/axiosWithAuth"
 import SignIn from "./components/SignIn"
 import FriendsList from "./components/FriendsList"
+import Logout from "./components/Logout"
 import ProtectedRoute from "./utils/ProtectedRoute"
 import './App.css';
 
 function App() {
+  const signedIn = getToken()
   return (
     <div className="App">
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/signin">Sign in</Link>
-        <Link to="/friends-list">Friends</Link>
-        <Link to="">Logout</Link>
+        {!signedIn && <Link to="/signin">Sign in</Link>}
+        {signedIn && <Link to="/friends-list">Friends</Link>}
+        {signedIn && <Link to="/logout">Logout</Link>}
       </nav>
       
       <Route exact path="/signin" component={SignIn} />
       <ProtectedRoute exact path="/friends-list" component={FriendsList} />
+      <ProtectedRoute exact path="/logout" component={Logout} />
     </div>
   );
 }
