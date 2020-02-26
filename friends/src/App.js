b321bd283719154c63d441e826003fdb84b0dab0
William Ryan <billy.ryan.rom@gmail.com>
2020-02-26 11:48:38 -0500

    Wrap app in BrowserRouter/Add Links for Components

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link, Route } from "react-router-dom"
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="">Sign in</Link>
+        <Link to="">Friends</Link>
+        <Link to="">Logout</Link>
+      </nav>
     </div>
   );
 }
